44b4ff89968f3c4395e3e1d8f04a404bbc95d1a0
philipp <mail@philippdumke.net>
2018-12-29 15:13:34 +0100

    GUI angefangen Testfälle hinzugefügt

--- ecc_tests.erl
+++ ecc_tests.erl
@@ -13,4 +13,14 @@ ublock_4_test() -> A = ecc:ublock("hallo 123",10,[]),
 ublock_5_test() -> A = ecc:ublock("😂",10,[]),
                    "😂" = string:strip(ecc:unblock(A,10,[]), right).
 
+euler_kriterium_test() ->  false = ecc:euler_kriterium(7,13).
+euler_kriterium_2_test() -> true = ecc:euler_kriterium(3,13).
+
+
+euklid_test() -> {-8,-3} = ecc:euklid({73,0},{27,1}).
+
+is_less_1_test() -> false = ecc:is_less({73,0},{42,1}).
+is_less_2_test() -> true = ecc:is_less({41,0},{42,1}).
+is_less_3_test() -> false = ecc:is_less({1,3},{1,2}).
+is_less_4_test() -> true = ecc:is_less({1,0},{1,1}).
 
--- gui.erl
+++ gui.erl
@@ -2,16 +2,73 @@
 -compile(export_all).
 -include_lib("wx/include/wx.hrl").
 
-
 start() ->
-	State = make_window(),
-	loop(State).
+    State = make_window(),
+    loop(State).
 
 make_window() ->
-	Server = wx:new(),
-	Frame = wxFrame:new(Server ,-1, "Ecc",[{size,{800,600}}]),
-	Panel = wxPanel:new(Frame),
-	
-	MainSizer = wxBoxSizer:new(?wxHORIZONTAL),
-	LeftSide = wxStaticBoxSizer:new(?wxVERTICAL),
-	RightSide = wxStaticBoxSizer:new(?wxVERTICAL),
+    Server = wx:new(),
+    Frame = wxFrame:new(Server, -1, "ECC", [{size,{1200, 800}}]),
+    Panel  = wxPanel:new(Frame),
+
+%% Erstellen der Sizer für das Layout
+    MainSizer = wxBoxSizer:new(?wxHORIZONTAL),
+    RightSide = wxStaticBoxSizer:new(?wxVERTICAL, Panel,[]),
+    LeftSide = wxStaticBoxSizer:new(?wxVERTICAL,Panel,[]),
+
+    Eingabe = wxStaticBoxSizer:new(?wxHORIZONTAL,Panel,[]),
+    EingabeHD = wxStaticBoxSizer:new(?wxVERTICAL,Panel,[]),
+    EingabeTL = wxStaticBoxSizer:new(?wxVERTICAL,Panel,[]),
+
+    Hash = wxStaticBoxSizer:new(?wxHORIZONTAL,Panel,[]),
+
+    %%  Eingabe
+    STEing1 = wxStaticText:new(Panel, 2001,"", []),
+    STEing2 = wxStaticText:new(Panel, 2002,"Eingabe", []),
+    STEing3 = wxStaticText:new(Panel, 2003,"", []),
+    STEing4 = wxStaticText:new(Panel, 2004,"", []),
+
+    TEingabe = wxTextCtrl:new(Panel, 1001,[{value, "lorem ipsum dolor"},{style,?wxDEFAULT bor ?wxTE_MULTILINE}]),
+    %% Hash
+    STHash1 = wxStaticText:new(Panel,2005, "   Hash        ",[]),
+    TEHash = wxTextCtrl:new(Panel,1002,[{value,""},{style,?wxDEFAULT bor ?wxTE_MULTILINE bor ?wxTE_READONLY}]),
+
+
+
+    % Eingabe Beschriftung
+    wxSizer:add(EingabeHD,STEing1,[{flag,?wxEXPAND}, {proportion,1}]),
+    wxSizer:add(EingabeHD,STEing2,[{flag,?wxEXPAND}, {proportion,1}]),
+    wxSizer:add(EingabeHD,STEing3,[{flag,?wxEXPAND}, {proportion,1}]),
+    wxSizer:add(EingabeHD,STEing4,[{flag,?wxEXPAND}, {proportion,1}]),
+
+    wxSizer:add(Eingabe,EingabeHD,[{flag, ?wxEXPAND}]),
+    wxSizer:add(Eingabe,TEingabe,[{flag,?wxEXPAND},{proportion,1}]),
+
+    wxSizer:add(Hash, STHash1,[]),
+    wxSizer:add(Hash,TEHash,[{flag,?wxEXPAND},{proportion,1}]),
+
+
+
+
+    wxSizer:add(LeftSide,Eingabe,[{flag,?wxEXPAND}]),
+    wxSizer:add(LeftSide,Hash,[{flag,?wxEXPAND}]),
+    wxSizer:add(MainSizer,LeftSide,[{flag,?wxEXPAND},{proportion,1}]),
+    wxSizer:add(MainSizer,RightSide,[{flag,?wxEXPAND},{proportion,1}]),
+
+    wxPanel:setSizer(Panel, MainSizer),
+    %% Frame anzeigen
+    wxFrame:show(Frame),
+    wxFrame:connect(Frame, close_window),
+    wxFrame:connect(Frame, command_button_clicked),
+
+    {Frame,TEingabe}.
+
+
+loop(State) ->
+    {Frame,T1001} = State,
+    receive
+        #wx{event=#wxClose{}} ->
+            wxWindow:destroy(Frame),
+            io:format("Exit Button klicked\n",[]),
+            ok
+    end.
